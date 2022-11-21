48946acdbabac55c8f8d28d43fa44f904c70c85d
Oriol Sors <oriolsors@gmail.com>
2022-11-21 16:27:23 +0100

    ImGui branch created. ImGui files and ModuleEditor created. Linker compiling issues

---
+++ ModuleEditor.cpp
@@ -0,0 +1,62 @@
+#include "ModuleEditor.h"
+#include "Application.h"
+#include "ModuleWindow.h"
+#include "ModuleRender.h"
+#include "Game/ImGui/imgui.h"
+#include "Game/ImGui/imgui_impl_sdl.h"
+#include "Game/ImGui/imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <SDL.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL_opengles2.h>
+#else
+#include <SDL_opengl.h>
+#endif
+
+ModuleEditor::ModuleEditor()
+{
+}
+
+ModuleEditor::~ModuleEditor()
+{
+}
+
+bool ModuleEditor::Init()
+{
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer->context);
+    ImGui_ImplOpenGL3_Init(GLSL_VERSION);
+    return true;
+}
+
+update_status ModuleEditor::PreUpdate()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL2_NewFrame(App->window->window);
+    ImGui::NewFrame();
+
+    ImGui::ShowDemoWindow();
+    return UPDATE_CONTINUE;
+}
+
+update_status ModuleEditor::Update()
+{
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return UPDATE_CONTINUE;
+}
+
+update_status ModuleEditor::PostUpdate()
+{
+    ImGui::EndFrame();
+    return UPDATE_CONTINUE;
+}
+
+bool ModuleEditor::CleanUp()
+{
+    return true;
+}
